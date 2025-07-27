11966972e7300234693be21071c27aeef88e9615
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-07-27 17:41:02 +0500

    init

---
+++ src/main/java/ru/netology/hw_jdbc/HwJdbcApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.hw_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwJdbcApplication.class, args);
+	}
+
+}
